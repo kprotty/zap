cf40843db33512844cf095bf287128a9935d7eda
kprotty <kbutcher6200@gmail.com>
2019-07-23 15:05:58 -0500

    More docs on Tokens

--- src/zio.zig
+++ src/zio.zig
@@ -7,8 +7,79 @@ const Impl = switch (builtin.os) {
     else => @compileError("Operating system not supported"),
 };
 
+/// Represents the underlying OS resource used to simulate Selectors, Sockets, Files and Timers
 pub const Handle = Impl.Handle;
 
+/// `Token` represents a unique non blocking IO Request which eventually returns an Token.Result.
+///   This is used to abstract over completion based selector apis (such as Windows' IOCP)
+///   as readiness/event based selectors (like select, poll, epoll, and kqueue) can be 
+///   interfaced in a completion based way while the opposite requires memory allocation.
+pub const Token = packed struct {
+    inner: Impl.Token,
+
+    /// Reset the token for a new IO request.
+    /// Should be called before passing into an IO function
+    pub inline fn reset(self: *Token) void {
+        return self.inner.reset();
+    }
+
+    /// View the identity of a token as a Reader.
+    /// Used for token equality when received from a `Selector.Event`.
+    pub inline fn asReader(self: *const Token) *const Token {
+        return self.inner.asReader();
+    }
+
+    /// View the identity of a token as a Writer.
+    /// Used for token equality when received from a `Selector.Event`.
+    pub inline fn asWriter(self: *const Token) *const Token {
+        return self.inner.asWriter();
+    }
+
+    /// Check if the identity of two tokens match.
+    /// Both `self` and `other` should be the result of either `asReader()` or `asWriter()`
+    pub inline fn is(self: *const Token, other: *const Token) bool {
+        return self.inner.is(other);
+    }
+
+    /// Get the `Token.Result` of a token retrieved from `Selector.Event.getToken()`.
+    /// If the token was not produced in that matter, the value of `getResult()` is undefined.
+    pub inline fn getResult(self: *Token) Result {
+        return self.inner.getResult();
+    }
+
+    /// Represents the result of an IO operation.
+    /// Each result type is handled differently based on where the Result was provided.
+    pub const Result = union(enum) {
+        /// There was an error processing the IO request on the API level.
+        /// The `u32` represents the amount of bytes that were transferred nonetheless.
+        Error: u32,
+
+        /// The IO operation request completed successfully.
+        /// The `u32` represents the amount of bytes that were transferred.
+        Transferred: u32,
+
+        /// If the result is from `Token.getResult()` (See above):
+        ///   the IO request was partially completed and
+        ///   the token should retry the operation where it left off to transfer fully.
+        /// If the result is from an IO operation:
+        ///   the buffers passed in are too large to be transffered.
+        ///   Try performing the operation in batches of `std.math.maxInt(u32)` bytes.
+        Retry,
+
+        /// If the result is from `Token.getResult()` (See above):
+        ///   this should be ignored as it shouldnt happen. Treat as error 
+        /// If the result is from an IO operation:
+        ///   the operation was partially completed with `u32` bytes transferred.
+        ///   The handle should call `Selector.register` to asynchronously wait for the completion event.
+        ///     if `RegisterRead`, the handle should be registered with `Selector.Event.Interest.Read`
+        ///     if `RegisterWrite`, the handle should be registered with `Selector.Event.Interest.Write`
+        RegisterRead: u32,
+        RegisterWrite: u32,
+    };
+};
+
+/// A Selector is an implementation of the kernel's IO multiplexer which is used
+/// to asynchronously process non blocking IO events such as Read and Write.
 pub const Selector = struct {
     inner: Impl.Selector,
 
@@ -17,30 +88,19 @@ pub const Selector = struct {
         return self.inner.getHandle();
     }
 
-    /// Type of IO event to register for polling.
-    /// Used in `register` to asynchronously wait for an event to complete/trigger.
-    pub const Interest = enum {
-        /// Listen for input events
-        Read,
-        /// Listen for output events
-        Write,
-        /// Listen for both input and output events
-        ReadWrite,
-    };
-
-    pub const Token = packed struct {
-        inner: Impl.Selector.Token,
-
-        // If tokens are valid to use on the current platform
-        pub const is_valid = Impl.Selector.Token.is_valid;
-
-        pub inline fn getResult(self: Token) IOResult {
-            return self.inner.getResult();
-        }
-    };
-
     pub const Event = packed struct {
         inner: Impl.Selector.Event,
+
+        /// Type of IO event to register for polling.
+        /// Used in `register`/`modify` to subscribe for an IO event when enqueued.
+        pub const Interest = enum {
+            /// Listen for input events
+            Read,
+            /// Listen for output events
+            Write,
+            /// Listen for both input and output events
+            ReadWrite,
+        };
 
         pub inline fn getToken(self: Event) ?*Token {
             return inner.getToken();
@@ -56,25 +116,38 @@ pub const Selector = struct {
         return Selector { .inner = inner };
     }
 
-    pub inline fn register(self: *Selector, socket: *Socket, interest: Interest, user_data: usize) ?void {
-        return self.inner.register(socket, interest, user_data);
+    pub inline fn register(
+        self: *Selector,
+        handle: Handle,
+        interest: Event.Interest,
+        user_data: usize,
+    ) ?void {
+        return self.inner.register(handle, interest, user_data);
     }
 
-    pub inline fn unregister(self: *Selector, socket: *Socket) ?void {
-        return self.inner.unregister(self, socket);
+    pub inline fn modify(
+        self: *Selector,
+        handle: Handle,
+        interest: Event.Interest,
+        user_data: usize,
+    ) ?void {
+        return self.inner.register(handle, interest, user_data);
     }
 
-    pub inline fn poll(self: *Selector, events: []Event, timeout_ms: ?u32) ?[]Event {
+    pub inline fn unregister(
+        self: *Selector,
+        handle: Handle,
+    ) ?void {
+        return self.inner.unregister(self, handle);
+    }
+
+    pub inline fn poll(
+        self: *Selector,
+        events: []Event,
+        timeout_ms: ?u32,
+    ) ?[]Event {
         return self.inner.poll(events, timeout_ms);
     }
-};
-
-pub const IOResult = union(enum) {
-    TooLarge,
-    Error: u32,
-    Transferred: u32,
-    RegisterRead: u32,
-    RegisterWrite: u32,
 };
 
 pub const Buffer = struct {
@@ -133,68 +206,68 @@ pub const Socket = struct {
     /// Read from the socket into a slice of buffers
     ///  `buffers` internal representation of a [][]u8 to read into
     ///  `address` if non null, read in address as well via a recvfrom() equivalent
-    ///  `token` if null, perform the operation synchronously. See more about tokens above
-    /// returns `IOResult` so actions in response to it should be taken accordingly (See above)
+    ///  `token` if null, perform the operation synchronously. See more about `Token` above
+    /// returns `Token.Result` so actions in response to it should be taken accordingly (See above)
     pub inline fn read(
         self: *Socket,
         buffers: []Buffer,
         address: ?*Address,
         token: ?*Token,
-    ) IOResult {
+    ) Token.Result {
         return self.inner.read(buffers, address, token);
     }
 
     /// Write into the socket using a slice of buffers
     ///  `buffers` internal representation of a [][]const u8 to write with
     ///  `address` if non null, send the address as well via a sendto() equivalent
-    ///  `token` if null, perform the operation synchronously. See more about tokens above.
-    /// returns `IOResult` so actions in response to it should be taken accordingly (See above)
+    ///  `token` if null, perform the operation synchronously. See more about `Token` above.
+    /// returns `Token.Result` so actions in response to it should be taken accordingly (See above)
     pub inline fn write(
         self: *Socket,
         buffers: []const Buffer,
         address: ?Address,
         token: ?*Token,
-    ) IOResult {
+    ) Token.Result {
         return self.inner.write(buffers, address, token);
     }
 
     /// Connect to a given address from a Protocol.Tcp socket.
     ///  `address` the address the attempt a connection with
-    ///  `token` if null, perform the operation synchronously. See more about tokens above.
-    /// returns `IOResult` with `Transferred` meaning successful connection (See more above).
+    ///  `token` if null, perform the operation synchronously. See more about `Token` above.
+    /// returns `Token.Result` with `Transferred` meaning successful connection (See more above).
     pub inline fn connect(
         self: *Socket,
         address: Address,
         token: ?*Token,
-    ) IOResult {
+    ) Token.Result {
         return self.inner.connect(address, token);
     }
 
     /// Accept an incoming client socket using the current Protocol.Tcp socket.
     ///  `address` the remote address of the client once accepted
     ///  `incoming` address to store the incoming socket once accepted (cannot be bind()'ed or connect()'ed)
-    ///  `token` if null, perform the operation synchronously. See more about tokens above.
-    /// returns `IOResult` with `Transferred` meaning successful acception (See more above).
+    ///  `token` if null, perform the operation synchronously. See more about `Token` above.
+    /// returns `Token.Result` with `Transferred` meaning successful acception (See more above).
     pub inline fn accept(
         self: *Socket,
         address: *Address,
         incoming: *Socket,
         token: ?*Token,
-    ) IOResult {
+    ) Token.Result {
         return self.inner.accept(self, address, incoming, token);
     }
 
     /// Transfer bytes from an OS handle to the socket without using an intermediary buffer.
     ///  `handle` the raw OS handle to internally read from
     ///  `transfer` amount of bytes to transfer
-    ///  `token` if null, perform the operation synchronously. See more about tokens above.
-    /// returns `IOResult` so actions in response to it should be taken accordingly (See above)
+    ///  `token` if null, perform the operation synchronously. See more about `Token` above.
+    /// returns `Token.Result` so actions in response to it should be taken accordingly (See above)
     pub inline fn sendfile(
         self: *Socket,
         handle: Handle,
         transfer: u32,
         token: ?*Token,
-    ) IOResult {
+    ) Token.Result {
         return self.inner.sendfile(self, handle, transfer, token);
     }
 

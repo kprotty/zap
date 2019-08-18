cc32d28f82c9464ad3c35bb2cd23e36cd8cbcc83
kprotty <kbutcher6200@gmail.com>
2019-08-18 01:19:52 -0500

    Delete build test generation

--- build.zig
+++ build.zig
@@ -17,23 +17,4 @@ pub fn build(b: *Builder) void {
         example.setBuildMode(build_mode);
         example_step.dependOn(&example.step);
     }
-
-    // setup tests
-    const test_all_step = b.step("test", "Run all tests in all modes");
-    inline for ([_]Mode { Mode.Debug, Mode.ReleaseSafe, Mode.ReleaseFast, Mode.ReleaseSmall }) |mode| {
-        const mode_str = switch (mode) {
-            .Debug => "debug",
-            .ReleaseSafe => "release-safe",
-            .ReleaseFast => "release-fast",
-            .ReleaseSmall => "release-small",
-        };
-
-        const tests = b.addTest("ziggo.zig");
-        tests.setBuildMode(mode);
-        tests.setNamePrefix(mode_str ++ " ");
-
-        const test_step = b.step("test-" ++ mode_str, "Run all tests in " ++ mode_str ++ ".");
-        test_step.dependOn(&tests.step);
-        test_all_step.dependOn(test_step);
-    }
 }

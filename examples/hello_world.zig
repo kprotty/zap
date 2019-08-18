6969c9c0251679b51b7f925da32ae1d47b76d980
kprotty <kbutcher6200@gmail.com>
2019-08-18 01:26:15 -0500

    Start runtime interface + hello world

--- examples/hello_world.zig
+++ examples/hello_world.zig
@@ -1,5 +1,11 @@
 const ziggo = @import("ziggo");
+const runtime = ziggo.runtime;
+const std = @import("std");
 
-pub fn main() void {
-    // TODO
+pub fn main() !void {
+    try runtime.run(runtime.Config.default(), start);
+}
+
+async fn start() void {
+    std.debug.warn("Hello world\n");
 }
--- ziggo.zig
+++ ziggo.zig
@@ -1,5 +1,13 @@
+const scheduler = @import("src/runtime/scheduler.zig");
 
 test "ziggo" {
     
 }
 
+pub const runtime = struct {
+    pub const Config = scheduler.Config;
+
+    pub fn run(config: Config, comptime main_function: var, main_args: ...) !void {
+        return scheduler.run(config, main_function, main_args);
+    }
+};

7906a94b2007e38cf37db8ba178894daa707526b
kprotty <kbutcher6200@gmail.com>
2019-08-27 14:43:05 -0500

    NUMA aware scheduler start

--- build.zig
+++ build.zig
@@ -1,20 +1,6 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const Mode = builtin.Mode;
-const Builder = std.build.Builder;
-
-pub fn build(b: *Builder) void {
-    const build_mode = b.standardReleaseOptions();
-
-    // setup examples
-    const example_step = b.step("examples", "Build examples");
-    inline for ([_][]const u8 {
-        "hello_world",
-    }) |example_name| {
-        const example = b.addExecutable(example_name, "examples/" ++ example_name ++ ".zig");
-        example.addPackagePath("ziggo", "ziggo.zig");
-        example.setBuildMode(build_mode);
-        example_step.dependOn(&example.step);
-    }
+pub fn build(b: *std.build.Builder) void {
+    
 }

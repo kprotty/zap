14356dcc75ca51ae43561054a0ab15d8d440b7a4
kprotty <kbutcher6200@gmail.com>
2019-09-08 14:18:55 -0500

    nextPowerOfTwo: add support for comptime_int

--- zync/src/utils.zig
+++ zync/src/utils.zig
@@ -8,11 +8,38 @@ pub fn CachePadded(comptime T: type) type {
     };
 }
 
+pub fn popCount(value: var) @typeOf(value) {
+    const T = @typeOf(value);
+    switch (@typeId(T)) {
+        .Int => return @popCount(T, value),
+        .ComptimeInt => {
+            comptime var bits = 0;
+            comptime var current = 0;
+            comptime var value_bits = value;
+            inline while (value_bits > 0) : (value_bits >>= 1) {
+                if ((value_bits & 1) != 0)
+                    bits = current;
+                current += 1;
+            }
+            return bits;
+        },
+        else => @compileError("Only supports integers"),
+    }
+}
+
 pub fn nextPowerOfTwo(value: var) @typeOf(value) {
     const T = @typeOf(value);
-    const Bits = @typeInfo(T).Int.bits;
-    const Shr = @log2(f32, @intToFloat(f32, Bits));
-    const ShrType = @IntType(false, @floatToInt(u64, Shr));
-    return T(1) << @truncate(ShrType, Bits - @clz(T, value - 1));
+    switch (@typeInfo(T)) {
+        .Int => |int| {
+            const ShrBits = @log2(f32, @intToFloat(f32, int.bits));
+            const ShrType = @IntType(false, @floatToInt(u64, ShrBits));
+            return T(1) << @truncate(ShrType, int.bits - @clz(T, value - 1));
+        },
+        .ComptimeInt => {
+            const power_of_two = comptime_int(1) << popCount(value);
+            return power_of_two << (if (power_of_two < value) 1 else 0);
+        },
+        else => @compileError("Only supports integers"),
+    }
 }
 

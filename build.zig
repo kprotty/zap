6bfd77d45d80a401ed33db5bf3fddf0216c73ac0
kprotty <kbutcher6200@gmail.com>
2019-10-13 19:05:07 -0500

    Update docs & test + fix CI

--- build.zig
+++ build.zig
@@ -2,11 +2,17 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn build(b: *std.build.Builder) void {
-    const test_zap = b.addTest("zap.zig");
-    test_zap.addPackagePath("zap", "zap.zig");
-    test_zap.setBuildMode(b.standardReleaseOptions());
-    
-    const test_step = b.step("test", "Run all tests");
-    test_step.dependOn(&test_zap.step);
-    b.default_step.dependOn(test_step);
+    const test_step = b.step("test", "Run all tests & generate docs");
+    test_step.dependOn(&b.addSystemCommand([_][]const u8 {
+        b.zig_exe,
+        "test",
+        "zap.zig",
+        "-femit-docs",
+        "--output-dir",
+        "zig-cache",
+    }).step);
+
+    const docs_step = b.addExecutable("docs", "docs.zig");
+    test_step.dependOn(&docs_step.step);
+    test_step.dependOn(&docs_step.run().step);
 }

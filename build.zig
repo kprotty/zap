67fedce392e643705a29fbc7e4cc0c87340ef6d6
kprotty <kbutcher6200@gmail.com>
2019-10-21 12:25:32 -0500

    Disable doc generation

--- build.zig
+++ build.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn build(b: *std.build.Builder) void {
+    const test_zap = b.addTest("zap.zig");
+    test_zap.setBuildMode(b.standardReleaseOptions());
+
+    const test_step = b.step("test", "Run all tests");
+    test_step.dependOn(&test_zap.step);
+}
+
+fn buildDocs(b: *std.build.Builder) void {
     const test_step = b.step("test", "Run all tests & generate docs");
     test_step.dependOn(&b.addSystemCommand([_][]const u8{
         b.zig_exe,

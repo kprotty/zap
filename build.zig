c46816b306ead91054e32a532820756f2df547ec
kprotty <kbutcher6200@gmail.com>
2019-10-24 10:21:17 -0500

    Proper build.zig with docgen

--- build.zig
+++ build.zig
@@ -2,27 +2,21 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn build(b: *std.build.Builder) void {
-    const test_zap = b.addTest("zap.zig");
-    test_zap.setBuildMode(b.standardReleaseOptions());
-
-    const test_step = b.step("test", "Run all tests");
-    test_step.dependOn(&test_zap.step);
-}
-
-fn buildDocs(b: *std.build.Builder) void {
-    const test_step = b.step("test", "Run all tests & generate docs");
-    test_step.dependOn(&b.addSystemCommand([_][]const u8{
+    const entry_point = "zap.zig";
+    const build_docs = b.addSystemCommand([_][]const u8 {
         b.zig_exe,
         "test",
-        "zap.zig",
+        entry_point,
         "-femit-docs",
+        "-fno-emit-bin",
         "--output-dir",
-        "zig-cache",
-    }).step);
+        ".",
+    });
 
-    const docs_step = b.addExecutable("docs", "docs.zig");
-    if (builtin.os != .windows)
-        docs_step.linkSystemLibrary("c");
-    test_step.dependOn(&docs_step.step);
-    test_step.dependOn(&docs_step.run().step);
+    const test_zap = b.addTest(entry_point);
+    test_zap.setBuildMode(b.standardReleaseOptions());
+
+    const test_step = b.step("test", "Run all tests & build docs");
+    test_step.dependOn(&test_zap.step);
+    test_step.dependOn(&build_docs.step);
 }

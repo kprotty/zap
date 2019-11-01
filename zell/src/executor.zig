bb435b76145828515d60722990d9b184ddb096dc
kprotty <kbutcher6200@gmail.com>
2019-11-01 12:03:04 -0500

    zell: manual Node initialization

--- zell/src/executor.zig
+++ zell/src/executor.zig
@@ -23,8 +23,17 @@ pub const Executor = struct {
     /// Ensures that the code being executed runs on a single thread
     /// *NOT* that the system doesnt spawn any other threads.
     pub fn runSequential(self: *@This(), comptime func: var, args: ...) !@typeOf(func).ReturnType {
-        var main_node = Node{};
-        try main_node.init(self, 0, [_]Worker{undefined});
+        var main_node = Node{
+            .executor = self,
+            .workers = [_]Worker { undefined },
+            .allocator = std.heap.direct_allocator, // TODO: better allocator
+            .affinity = zuma.CpuAffinity {
+                .group = 0,
+                .mask = 0,
+            },
+        };
+
+        try main_node.init(self, Thread.MAX);
         defer main_node.deinit();
         return self.runWithNodes([_]*Node{&main_node}, func, args);
     }
@@ -37,19 +46,19 @@ pub const Executor = struct {
         const num_nodes = zuma.CpuAffinity.getNodeCount();
         var node_array = [_]?*Node{null} ** Node.MAX;
         const nodes = node_array[0..num_nodes];
-        
+
         // Setup the defer to free the nodes first.
         // This is to ensure previously allocated nodes are cleaned up in case of error in the alloc loop.
         defer for (nodes) |node_ref|
             if (node_ref) |node|
                 node.free();
-        for (nodes) |*node_ref, numa_node|
-            node_ref.* = try Node.alloc(self, numa_node);
+        for (nodes) |*node_ref, node_index|
+            node_ref.* = try Node.alloc(self, node_index);
         const real_nodes = @ptrCast([*]*Node, nodes.ptr)[0..nodes.len];
         return self.runWithNodes(real_nodes, func, args);
     }
 
-    fn runWithNodes(self: *@This(), nodes: []*Node, comptime func: var, args: ...) !@typeOf(func).ReturnType {
+    pub fn runUsing(self: *@This(), nodes: []*Node, comptime func: var, args: ...) !@typeOf(func).ReturnType {
         return error.Todo;
     }
 };
@@ -59,19 +68,26 @@ pub const Node = struct {
     pub const MAX = 64;
 
     executor: *Executor,
-    numa_node: u32,
-    workers: []Worker,
-    reactor: zell.Reactor,
+    affinity: zuma.CpuAffinity,
+    allocator: *std.mem.Allocator,
+    reactor: zell.Reactor = undefined,
 
-    pub fn init(self: *@This(), executor: *Executor, numa_node: u32, workers: []Worker) !void {
-        self.workers = workers;
-        self.executor = executor;
-        self.numa_node = numa_node;
+    workers: []Worker,
+    thread_pool: Thread.Pool = undefined,
+
+    pub fn init(self: *@This(), max_threads: usize) !void {
+        try self.reactor.init(self.allocator);
+        try self.thread_pool.init(self.allocator);
     }
 
-    pub fn deinit(self: *@This()) void {}
+    pub fn deinit(self: *@This()) void {
+        self.reactor.deinit();
+        self.thread_pool.deinit();
+    }
 
-    pub fn alloc(executor: *Executor, numa_node: u32) !*@This() {}
+    pub fn alloc(executor: *Executor, numa_node: u32) !*@This() {
+        return error.Todo;
+    }
 
     pub fn free(self: *@This()) void {
         self.deinit();
@@ -86,6 +102,18 @@ pub const Worker = struct {
 pub const Thread = struct {
     /// Maximum number of Operating System Threads running concurrently in one Node
     pub const MAX = 10 * 1000;
+
+    pub threadlocal var current: ?*@This() = null;
+
+    pub const Pool = struct {
+        pub fn init(self: *@This(), allocator: *std.mem.Allocator) !void {
+            return error.Todo;
+        }
+
+        pub fn deinit(self: *@This()) void {
+
+        }
+    };
 };
 
 pub const Task = struct {

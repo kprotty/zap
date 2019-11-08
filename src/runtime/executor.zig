b8d69ee7e2b6119755b751eb58fa23422e8a9b08
kprotty <kbutcher6200@gmail.com>
2019-11-08 14:24:13 -0600

    Started structuring thread blocking

--- src/runtime/executor.zig
+++ src/runtime/executor.zig
@@ -2,36 +2,43 @@ const std = @import("std");
 const builtin = @import("builtin");
 const sync = @import("./sync.zig");
 
+const system = switch (builtin.os) {
+    .windows => @import("./system/windows.zig"),
+    .linux => @import("./system/linux.zig"),
+    .posix => @import("./system/posix.zig"),
+};
+
 pub const Executor = struct {
-    pending_tasks: usize,
     nodes: []*Node,
-    idle_nodes: sync.BitSet(usize),
+    idle_nodes: sync.BitSet,
+    pending_tasks: usize,
+    active_workers: sync.Barrier,
 
     pub fn run(comptime func: var, args: ...) !void {
-        var self: @This() = undefined;
+        var self: Executor = undefined;
         if (builtin.single_threaded)
             return self.runSequential(func, args);
         return self.runParallel(func, args);
     }
 
-    pub fn runSequential(self: *@This(), comptime func: var, args: ...) !void {
+    pub fn runSequential(self: *Executor, comptime func: var, args: ...) !void {
         // setup the worker on the stack
         var main_worker: Worker = undefined;
         var workers = [_]*Worker{ &main_worker };
 
         // setup the node on the stack
-        var main_node = try Node.init(self, workers[0..]);
+        var main_node = try Node.init(self, 0, 1, workers[0..]);
         defer main_node.deinit();
         var nodes = [_]*Node{ &main_node };
         return self.runUsing(nodes, func, args);
     }
 
-    pub fn runParallel(self: *@This(), comptime func: var, args: ...) !void {
+    pub fn runParallel(self: *Executor, comptime func: var, args: ...) !void {
         // store the array of node pointers on the stack.
         // since its on the stack, limit the maximum amount of numa nodes.
         // also perform the `defer` first to make sure all nodes are freed in case of error
-        var node_array: [_]?*Node{null} ** @typeInfo(usize).Int.bits;
-        const array = node_array[0..system.Thread.getNumaNodeCount()];
+        var node_array: [_]?*Node{null} ** BitSet.Max;
+        const array = node_array[0..system.getNumaNodeCount()];
         defer for (array) |ptr| {
             if (ptr) |node|
                 node.free();
@@ -39,52 +46,139 @@ pub const Executor = struct {
 
         // Allocate each node on its corresponding numa node for locality
         for (array) |*node, index|
-            node.* = try Node.alloc(self, index);
+            node.* = try Node.alloc(self, index, 1024 + 1);
         const nodes = @ptrCast([*]Node, array.ptr)[0..array.len];
         return self.runUsing(nodes, func, args);
     }
 
-    pub fn runUsing(self: *@This(), nodes: []*Node, comptime func: var, args: ...) !void {
-        self.nodes = nodes;
-        self.pending_tasks = 0;
-        self.idle_nodes = sync.BitSet(usize).init(node.len);
-        _ = async func(args);
+    pub fn runUsing(self: *Executor, nodes: []*Node, comptime func: var, args: ...) !void {
+        self.* = Executor{
+            .nodes = nodes,
+            .idle_nodes = sync.BitSet.init(nodes.len),
+            .pending_tasks = 0,
+            .active_workers = sync.Barrier.init(1),
+        };
         
         // start with a random numa node in order to not over-subscribe on a single
         // one if theres multiple numa applications running on the system.
-        const start_node = &nodes[system.getRandom().uintAtMost(usize, nodes.len)];
-        start_node.run();
+        const main_node = &nodes[system.getRandom().uintAtMost(usize, nodes.len)];
+        const main_worker = main_node.getIdleWorker().?;
+        var main_thread = Thread{
+            .node = main_node,
+            .link = null,
+            .worker = main_worker,
+        };
+
+        Worker.current = main_worker;
+        _ = async func(args);
+        var main_stack: [Worker.STACK_SIZE]u8 align(Worker.STACK_ALIGN) = undefined;
+        main_worker.stack = main_stack[0..];
+        main_thread.run();
+
+        self.active_workers.wait();
+        for (nodes) |node| {
+            while (node.thread_pool.pop()) |thread| {
+                thread.wakeup(null);
+            }
+        }
     }
 };
 
 pub const Node = struct {
+    numa_node: u16,
     executor: *Executor,
+    monitor: Thread.Monitor,
+    
     workers: []*Worker,
-    idle_workers: usize,
+    idle_workers: sync.BitSet,
+    thread_pool: Thread.Pool,
 
-    pub fn init(executor: *Executor, workers: []*Worker) !@This() {
-
-    }
-
-    pub fn deinit(self: *@This()) void {
+    pub fn init(executor: *Executor, numa_node: u16, max_threads: usize, workers: []*Worker) !Node {
 
     }
 
-    pub fn alloc(executor: *Executor, numa_node: u16) !*@This() {
+    pub fn deinit(self: *Node) void {
 
     }
 
-    pub fn free(self: *@This()) void {
+    pub fn alloc(executor: *Executor, numa_node: u16, max_threads: usize) !*Node {
 
+    }
+
+    pub fn free(self: *Node) void {
+
+    }
+
+    fn getIdleWorker(self: *Node) ?*Worker {
+        return null; // TODO
     }
 };
 
-pub const Worker = struct {
+const Thread = struct {
     node: *Node,
+    link: ?*Thread,
+    worker: ?*Worker,
+    parker: std.ThreadParker,
+
+    pub fn wake(self: *@This(), worker: ?*Worker) void {
+        if (@atomicRmw(?*Worker, &self.worker, .Xchg, worker, .Release) == null)
+            self.parker.unpark(@ptrCast(*const u32, &self.worker));
+    }
+
+    const Monitor = struct {
+
+    };
+
+    const Pool = struct {
+
+    };
+};
+
+pub const Worker = struct {
+    pub threadlocal var current: ?*Worker= null;
+
+    const STACK_ALIGN = if (builtin.os == .windows) 0 else std.mem.page_size;
+    const STACK_SIZE = switch (builtin.os) {
+        .windows => 0, // windows doesnt allow custom thread stacks
+        else => switch (@typeInfo(usize).Int.bits) {
+            64 => 8 * 1024 * 1024,
+            32 => 2 * 1024 * 1024,
+            else => @compileError("Architecture not supported"),
+        },
+    };
+
+    node: *Node,
+    thread: ?*Thread,
     run_queue: LocalQueue,
+    blocking_task: ?*Task,
+    stack: []align(STACK_ALIGN) u8,
+
+    pub fn init(node: *Node, stack: []align(STACK_ALIGN) u8) Worker {
+        return Worker{
+            .node = node,
+            .thread = null,
+            .run_queue = LocalQueue{},
+            .blocking_task = null,
+            .stack = stack,
+        };
+    }
+
+    pub fn submit(self: *Worker, task: *Task) void {
+
+    }
+
+    pub fn blocking(self: *Worker, comptime func: var, args: ...) @typeOf(func).ReturnType {
+        if (builtin.single_threaded or self.node.workers.len == 1)
+            return func(args);
+        // TODO
+    }
+
+    fn run(self: *Worker) void {
+        
+    }
 
     const GlobalQueue = struct {
-        mutex: sync.Mutex = sync.Mutex.init(),
+        mutex: std.Mutex = std.Mutex.init(),
         tasks: Task.List = Task.List{},
     };
 
@@ -97,28 +191,54 @@ pub const Worker = struct {
     };
 };
 
-const Thread = struct {
-    threadlocal var current = @This(){};
-
-    worker: ?*Worker,
-};
-
-const Task = struct {
+pub const Task = struct {
     next: ?*@This() = null,
     frame: anyframe = undefined,
 
-    const List = struct {
+    pub const Priority = enum(u2) {
+        /// Schedule to end of the local queue on the current node
+        Low,
+        /// Schedule to the front of the local queue on the current node
+        High,
+        /// Distribute to other workers or nodes
+        Root,
+    };
+
+    pub fn init(frame: anyframe, comptime priority: Priority) @This() {
+        return @This(){
+            .next = null,
+            .frame = @intToPtr(anyframe, @ptrToInt(frame) | @enumToInt(priority));
+        };
+    }
+
+    pub fn getFrame(self: @This()) anyframe {
+        return @intToPtr(anyframe, @ptrToInt(self.frame) & ~usize(~@TagType(Priority)(0)));
+    }
+
+    pub fn getPriority(self: @This()) Priority {
+        return @intToEnum(Priority, @truncate(@TagType(Priority), @ptrToInt(self.frame)));
+    }
+
+    pub const List = struct {
         size: usize = 0,
         head: ?*Task = null,
         tail: ?*Task = null,
         
-        fn push(self: *@This(), list: @This()) void {
+        pub fn push(self: *@This(), list: @This()) void {
             if (self.tail) |tail|
                 tail.next = list.head;
             self.tail = list.tail;
             if (self.head == null)
                 self.head = list.head;
             self.size += list.size;
+        }
+
+        pub fn pop(self: *@This()) ?*Task {
+            const head = self.head orelse return null;
+            self.head = head.next;
+            if (self.head == null)
+                self.tail = null;
+            return head;
         }
     };
 };
--- src/runtime/sync.zig
+++ src/runtime/sync.zig
@@ -1,157 +1,100 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const Futex = @import("./futex.zig").Futex;
-const Thread = @import("./thread.zig").Thread;
 
-/// TODO: https://github.com/ziglang/zig/issues/2995
 pub fn atomicStore(ptr: var, value: var, comptime order: builtin.AtomicOrder) void {
+    // TODO: https://github.com/ziglang/zig/issues/2995
     _ = @atomicRmw(@typeOf(ptr.*), ptr, .Xchg, value, order);
 }
 
-pub fn yield(spin_count: usize) {
-    var spin = spin_count;
-    while (spin != 0) : (spin -= 1) {
-        switch (builtin.arch) {
-            .i386, .x86_64 => asm volatile("pause" ::: "memory"),
-            .arm, .aarch64 => asm volatile("yield"),
-            else => Thread.yield(),
+pub const BitSet = struct {
+    const Word = usize;
+    const Max = @typeOf(Word).Int.bits;
+    const Index = @Type(builtin.TypeInfo{
+        .Int = builtin.TypeInfo.Int{
+            .is_signed = false,
+            .bits = @ctz(u16, Max),
         }
-    }
-}
+    });
 
-pub fn BitSet(comptime Word: type) type {
-    comptime std.debug.assert(
-        @typeId(Word) == builtin.TypeId.Int and
-        @typeInfo(Workd).Int.is_signed == false and
-        @sizeOf(Word) <= usize and @sizeOf(Word) > 0 and @sizeOf(Word) % 8 == 0
-    );
+    mask: Word,
 
-    return struct {
-        pub const Max = @typeInfo(Word).Int.bits;
-        pub const Index = @Type(builtin.TypeInfo{
-            .Int = builtin.TypeInfo.Int{
-                .is_singned = false,
-                .bits = @ctz(u8, Max),
-            },
-        });
-
-        mask: Word,
-
-        pub fn init(size: var) @This() {
-            const mask = if (size >= Max) ~Word(0) else 1 << @truncate(Index, size);
-            return @This(){ .mask = mask };
-        }
-
-        pub fn count(self: @This()) Word {
-            return @popCount(Word, @atomicLoad(Word, &self.mask, .Monotonic));
-        }
-
-        pub fn set(self: *@This(), index: usize) void {
-            _ = @atomicRmw(Word, &self.mask, .Or, @shlExact(Word(1), @truncate(Index, index)), .Release);
-        }
-
-        pub fn get(self: *@This()) ?Index {
-            var backoff: u5 = 0;
-            var mask = @atomicLoad(Word, &self.mask, .Monotonic);
-            while (mask != ~Word(0)) {
-                const index = @truncate(Index, @ctz(Word, ~mask));
-                const updated = mask & ~@shlExact(Word(1), index);
-                mask = @cmpxchgWeak(Word, &self.mask, mask, updated .Acquire, .Monotonic) orelse return index;
-                yield();
-            }
-            return null;
-        }
-    };
-}
-
-pub const Event = struct {
-    futex: Futex,
-    is_signaled: bool align(@alignOf(u32)),
-
-    const True = u32(@boolToInt(true));
-    const False = u32(@boolToInt(false));
-    
-    pub fn init() @This() {
+    pub fn init(size: usize) @This() {
         return @This(){
-            .futex = Futex.init(),
-            .is_signaled = false,
+            .mask = if (size >= MAX) ~Word(0) else (Word(1) << @truncate(Index, size)) - 1;
         };
     }
 
-    pub fn deinit(self: *@This()) void {
-        self.futex.deinit();
+    pub fn set(self: *@This(), index: usize) void {
+        _ = @atomicRmw(Word, &self.mask, .Or, 1 << @truncate(Index, index), .Release);
     }
 
-    pub fn set(self: *@This()) void {
-        if (@atomicRmw(u32, @ptrCast(*u32, &self.is_signaled), .Xchg, 1, True, .Release) == False)
-            self.futex.wake(@ptrCast(*const u32, &self.is_signaled));
-    }
-
-    pub fn reset(self: *@This()) void {
-        atomicStore(@ptrCast(*u32, &self.is_signaled), False, .Monotonic);
-    }
-
-    pub fn isSet(self: *const @This()) bool {
-        return @atomicLoad(u32, @ptrCast(*const u32, &self.is_signaled), .Monotonic) == True;
-    }
-
-    pub fn wait(self: *const @This()) void {
-        while (!self.isSet())
-            self.futex.wait(@ptrCast(*const u32, &self.is_signaled), False);
+    pub fn get(self: *@This()) ?Index {
+        var mask = @atomicLoad(Word, &self.mask, .Monotonic);
+        while (mask != ~Word(0)) : (std.SpinLock.yield(1)) {
+            const index = @truncate(Index, @ctz(Word, ~mask));
+            const updated = mask & ~(Word(1) << index);
+            mask = @cmpxchgWeak(Word, &self.mask, mask, updated, .Acquire, .Monotonic)
+                orelse return index;
+        }
+        return null;
     }
 };
 
-const Mutex = struct {
-    state: u32,
-    futex: Futex,
-    
-    const Unlocked = 0;
-    const Locked = 1;
-    const Sleeping = 2;
+pub const Barrier = struct {
+    count: u32,
+    parker: std.ThreadParker,
 
-    const SpinCpu = 4;
-    const SpinThread = 1;
-
-    pub fn init() @This() {
+    pub fn init(count: u32) @This() {
         return @This(){
-            .state = Unlocked,
-            .futex = Futex.init(),
+            .count = value,
+            .parker = std.ThreadParker.init(),
         };
-    }
-
-    pub fn deinit(self: *@This()) void {
-        self.futex.deinit();
     }
 
     pub fn acquire(self: *@This()) void {
-        var state = @atomicRmw(u32, &self.state, .Xchg, Locked, .Acquire);
-        if (state == Unlocked)
-            return;
-
-        while (true) {
-            for (([SpinCpu]void)(undefined)) |_, index| {
-                var value = @atomicLoad(u32, &self.state, .Monotonic);
-                while (value == Unlocked)
-                    value = @cmpxchgWeak(u32, &self.state, Unlocked, state, .Acquire, .Relaxed) orelse return;
-                yield(1 << @truncate(u5, index));
-            }
-
-            for (([SpinThread]void)(undefined)) |_| {
-                var value = @atomicLoad(u32, &self.state, .Monotonic);
-                while (value == Unlocked)
-                    value = @cmpxchgWeak(u32, &self.state, Unlocked, state, .Acquire, .Relaxed) orelse return;
-                Thread.yield();
-            }
-
-            if (@atomicRmw(u32, &self.state, .Xchg, Sleeping, .Acquire) == Unlocked)
-                return;
-            state = Sleeping;
-            self.futex.wait(&self.state, Sleeping);
-        }
+        _ = @atomicRmw(u32, &self.count, .Add, 1, .Acquire);
     }
 
     pub fn release(self: *@This()) void {
-        if (@atomicRmw(u32, &self.state, .Xchg, Unlocked, .Release) == Sleeping)
-            self.futex.wake(&self.state);
+        if (@atomicRmw(u32, &self.count, .Sub, 1, .Release) == 1)
+            self.parker.unpark(&self.count);
+    }
+
+    pub fn wait(self: *const @This()) void {
+        var count = @atomicLoad(u32, &self.count, .Monotonic);
+        while (count != 0) {
+            self.parker.park(&self.count, count);
+            count = @atomicLoad(u32, &self.count, .Monotonic);
+        }
     }
 };
+
+pub fn Lazy(comptime initializer: var) type {
+    return struct {
+        value: Type,
+        mutex: std.Mutex,
+        is_initialized: u8,
+
+        pub const Type = @typeOf(initializer).ReturnType;
+
+        pub fn init() @This() {
+            return @This(){
+                .value = null,
+                .is_initialized = 0,
+                .mutex = std.Mutex.init(),
+            };
+        }
+
+        pub fn get(self: *@This()) Type {
+            if (@atomicLoad(u8, &self.is_initialized, .Acquire) == 0) {
+                const held = self.mutex.acquire();
+                defer held.release();
+                if (@atomicLoad(u8, &self.is_initialized, .Monotonic) != 0)
+                    return self.value;
+                self.value = initializer();
+                atomicStore(&self.is_initialized, 1, .Release);
+            }
+            return self.value;
+        }
+    };
+}

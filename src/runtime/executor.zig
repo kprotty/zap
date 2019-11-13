54edb7a0e98c619a2a388b8b7a4ff8a079fb01c8
kprotty <kbutcher6200@gmail.com>
2019-11-13 15:51:32 -0600

    Preparing for no var args :(

--- src/runtime/executor.zig
+++ src/runtime/executor.zig
@@ -1,60 +1,102 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub const Executor = struct {
+    threadlocal var node_array: [64]?*Node = undefined;
+
     nodes: []*Node,
-    next_node: usize,
+    next_node: u32,
+    active_workers: u32,
     active_tasks: usize,
-    active_workers: usize,
+
+    pub fn init() !Executor {
+        if (builtin.single_threaded) {
+            return initSequential();
+        } else {
+            return initParallel();
+        }
+    }
+
+    pub fn initSequential() !Executor {
+        var workers = [_]Worker{undefined};
+        var node = Node.init(workers[0..], null);
+        var nodes = [_]*Node{ &node };
+        return initUsing(nodes[0..]);
+    }
+
+    pub fn initParallel() !Executor {
+        if (builtin.single_threaded)
+            @compileError("--single-threaded doesn't support parallel execution");
+            
+        const node_count = std.math.min(system.getNodeCount(), node_array.len);
+        const nodes = @ptrCast([*]*Node, &node_array[0])[0..node_count];
+        const node_slice = node_array[0..node_count];
+        
+        for (node_slice) |*ptr| {
+            ptr.* = null;
+        }
+        errdefer for (node_slice) |*ptr| {
+            if (ptr.*) |node| {
+                node.free();
+            }
+        }
+        for (node_slice) |*ptr, index| {
+            ptr.* = try Node.alloc(index, null, null);
+        }
+
+        return initUsing(nodes);
+    }
+
+    pub fn initSMP(max_workers: usize, max_threads: usize) !Executor {
+        const node = try Node.alloc(0, max_workers, max_threads);
+        errdefer node.free();
+        var nodes = [_]*Node{ node };
+        return initUsing(nodes[0..]);
+    }
+
+    pub fn initUsing(nodes: []*Node) !Executor {
+        
+    }
+
+    pub fn deinit(self: *Executor) void {
+
+    }
+
+    pub fn run(self: *Executor) void {
+        for (self.nodes) |node|
+            node.executor = self;
+    }
 };
 
 pub const Node = extern struct {
-    // thread pool cache-line
-    thread_mutex: u32 align(64),
-    free_count: u16,
-    num_threads: u16,
-    num_workers: u16,
-    stack_ptr: u16,
-    stack_end: u16,
-    stack_size: u16,
-    stack_top: ?*usize,
-    free_thread: ?*Thread,
     executor: *Executor,
-    idle_workers: usize,
 
-    // thread-exit cache-lines
-    exit_mutex: u32 align(64),
-    exit_stack: [128 - 16]u8 align(16),
+    pub fn init(workers: []Worker, thread_stack: ?[]u8) !Node {
 
-    runq_mutex: u32,
-    runq_size: u32,
-    runq_head: ?*Task,
-    runq_tail: ?*Task,
-    
+    }
+
+    pub fn deinit(self: *Node) void {
+
+    }
+
+    pub fn alloc(numa_node: u32, max_workers: ?usize, max_threads: ?usize) !*Node {
+        
+    }
+
+    pub fn free(self: *Node) void {
+
+    }
 };
 
 const Thread = struct {
     pub threadlocal var current: ?*Thread = null;
 
-    next: ?*Thread,
-    node: *Node,
-    worker: *Worker,
-
-
 };
 
 const Worker = struct {
     const STACK_ALIGN = 64 * 1024;
-    const STACK_SIZE = 4 * 1024 * 1024;
+    const STACK_SIZE = 1 * 1024 * 1024;
 
-    runq_head: u32,
-    runq_tail: u32,
-    runq_tick: usize,
-    runq_next: ?*Task,
-    thread: ?*Thread,
-    block_expires: u64,
-    node: *Node,
-    stack: [*]align(STACK_ALIGN) u8,
-    reserved: usize,
 };
 
 const Task = struct {

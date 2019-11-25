0d1e0e435bb8bddda205cb20bf0ca78227030988
kprotty <kbutcher6200@gmail.com>
2019-11-25 14:35:03 -0600

    Make it compatible with std.event.Loop start()

--- src/runtime/executor.zig
+++ src/runtime/executor.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub const Executor = struct {
-    
+    allocator: ?*std.mem.Allocator,
     workers: []Worker,
     active_tasks: usize,
     idle_workers: ?*Worker,
@@ -18,18 +18,60 @@ pub const Executor = struct {
     monitor_thread: ?*Thread,
     monitor_tick: u64,
 
-    pub fn run(comptime entryFn: var, args: ...) !@typeOf(entryFn).ReturnType {
+    pub fn init(self: *Executor) !void {
         if (builtin.single_threaded)
-            return runUsing(1, 1, entryFn, args);
-
-        const num_threads = 1024; // arbitrary
-        const num_cpus = try std.Thread.cpuCount(); 
-        return runUsing(num_cpus, num_threads, entryFn, args);
+            return self.initSequential();
+        
+        const max_threads = 1024;
+        const max_workers = try std.Thread.cpuCount();
+        return self.initParallel(max_workers, max_threads);
     }
 
-    pub fn runUsing(max_workers: usize, max_threads: usize, comptime entryFn: var, args: ...) !@typeOf(entryFn).ReturnType {
+    pub fn initSequential(self: *Executor) !void {
+        return self.initUsing(([_]Worker{ undefined })[0..], 1, null);
+    }
+
+    pub fn initParallel(self: *Executor, max_workers: usize, max_threads: usize) !void {
         const num_threads = std.math.max(1, max_threads);
-        const num_workers = std.math.min(num_threads, std.math.max(1, num_workers));
+        const num_workers = std.math.min(num_threads, std.math.max(1, max_workers));
+        if (num_threads == 1)
+            return self.initSequential();
+
+        // TODO: Use a better allocator
+        const allocator = if (builtin.link_libc) std.heap.c_allocator else std.heap.direct_allocator;
+        const workers = try allocator.alloc(Worker, num_workers);
+        return self.initUsing(workers, num_threads, allocator);
+    }
+
+    pub fn initUsing(self: *Executor, workers: []Worker, max_threads: usize, allocator: ?*std.mem.Allocator) !void {
+        self.* = Executor{
+            .allocator = allocator,
+            .workers = workers,
+            .active_tasks = 0,
+            .idle_workers = null,
+            .runq_lock = std.Mutex.init(),
+            .runq_size = 0,
+            .runq_head = null,
+            .runq_tail = null,
+            .thread_lock = std.Mutex.init(),
+            .idle_threads = null,
+            .free_threads = max_threads,
+            .monitor_thread = null,
+            .monitor_tick = 0,
+        };
+    }
+
+    pub fn deinit(self: *Executor) void {
+        if (self.allocator) |allocator| {
+            allocator.free(self.workers);
+        }
+    }
+
+    pub fn beginOneEvent(self: *Executor) void {}
+    pub fn finishOneEvent(self: *Executor) void {}
+
+    pub fn run(self: *Executor) void {
+
     }
 };
 

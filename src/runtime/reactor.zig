0560546b09b37d030079c665f08fb1a171607b91
kprotty <kbutcher6200@gmail.com>
2019-11-27 10:47:31 -0600

    designing reactor interface

---
+++ src/runtime/reactor.zig
@@ -0,0 +1,102 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const os = std.os;
+const net = std.net;
+
+const backend = switch (builtin.os) {
+    .windows => @import("reactor/windows.zig"),
+    .linux => @import("reactor/linux.zig"),
+    else => @import("reactor/bsd.zig"),
+};
+
+pub const Descriptor = struct {
+    inner: backend.Descriptor,
+
+    pub fn getHandle(self: Descriptor) os.fd_t {
+        return self.inner.getHandle();
+    }
+};
+
+pub const Reactor = struct {
+    inner: backend.Reactor,
+
+    pub const Error = error{
+
+    };
+
+    pub fn init() Error!Reactor {
+        return Reactor{ .inner = backend.Reactor.init() };
+    }
+
+    pub fn deinit(self: *Reactor) void {
+        return self.inner.deinit();
+    }
+
+    pub const RegisterError = error{
+
+    };
+
+    pub fn register(self: *Reactor, handle: os.fd_t) RegisterError!*Descriptor {
+        return @ptrCast(*Descriptor, try self.inner.register(handle));
+    }
+
+    pub fn close(self: *Reactor, descriptor: *Descriptor) void {
+        return self.inner.close(&descriptor.inner);
+    }
+
+    pub const AcceptError = error{
+
+    };
+
+    pub fn accept(self: *Reactor, descriptor: *Descriptor, address: *net.Address) AcceptError!*Descriptor {
+        return @ptrCast(*Descriptor, try self.inner.accept(address, &descriptor.inner));
+    }
+
+    pub const ConnectError = error{
+
+    };
+
+    pub fn connect(self: *Reactor, descriptor: *Descriptor, address: *const net.Address) ConnectError!void {
+        return self.inner.connect(&descriptor.inner, address);
+    }
+
+    pub const ReadError = error{
+
+    };
+
+    pub fn read(self: *Reactor, descriptor: *Descriptor, data: []const os.iovec, address: ?*net.Address) ReadError!usize {
+        return self.inner.read(&descriptor.inner, data, address);
+    }
+
+    pub const WriteError = error{
+
+    };
+
+    pub fn write(self: *Reactor, descriptor: *Descriptor, data: []const os.iovec_const, address: ?*const net.Address) WriteError!usize {
+        return self.inner.write(&descriptor.inner, data, address);
+    }
+
+    pub const FsyncError = error{
+
+    };
+
+    pub fn fsync(self: *Reactor, descriptor: *Descriptor) FsyncError!void {
+        return self.inner.fsync(&descriptor.inner);
+    }
+
+    pub const FileReadError = error{
+
+    };
+
+    pub fn fread(self: *Reactor, descriptor: *Descriptor, data: []const os.iovec, offset: ?u64) FileReadError!usize {
+        return self.inner.fread(&descriptor.inner, data, offset);
+    }
+
+    pub const FileWriteError = error{
+
+    };
+
+    pub fn fwrite(self: *Reactor, descriptor: *Descriptor, data: []const os.iovec_const, offset: ?u64) FileWriteError!usize {
+        return self.inner.fwrite(&descriptor.inner, data, offset);
+    }
+};

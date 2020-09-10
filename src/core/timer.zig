aecb976e92e3dfa8f816a07730d8c29f882157b4
kprotty <kbutcher6200@gmail.com>
2020-09-10 12:37:01 -0500

    timing wheel test cases pass

--- src/core/timer.zig
+++ src/core/timer.zig
@@ -1,3 +1,8 @@
+// =============================================================
+// Zig port of timeout.c - Tickless hierarchical timing wheel.
+// https://github.com/wahern/timeout/blob/master/timeout.c
+// =============================================================
+
 // Copyright (c) 2020 kprotty
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,271 +21,325 @@ const std = @import("std");
 
 const min = std.math.min;
 const max = std.math.max;
-const rotl = std.math.rotl;
-const rotr = std.math.rotr;
-const maxInt = std.math.maxInt;
-const Log2Int = std.math.Log2Int;
 const IntType = std.meta.Int;
+const Log2Int = std.math.Log2Int;
+const bitCount = std.meta.bitCount;
 
 fn boolToInt(b: bool) u1 {
     return @boolToInt(b);
+}
+
+fn cast(comptime T: type, n: anytype) T {
+    return @intCast(T, n);
+}
+
+fn truncate(comptime T: type, n: anytype) T {
+    return @truncate(T, n);
 }
 
 fn ctz(n: anytype) @TypeOf(n) {
     return @ctz(@TypeOf(n), n);
 }
 
-fn fls(n: anytype) usize {
-    return std.meta.bitCount(@TypeOf(n)) - @clz(@TypeOf(n), n);
+fn clz(n: anytype) @TypeOf(n) {
+    return @clz(@TypeOf(n), n);
 }
 
-fn as(comptime Int: type, value: anytype) Int {
-    return @as(Int, value);
+fn fls(n: anytype) @TypeOf(n) {
+    return bitCount(@TypeOf(n)) - clz(n);
 }
 
-fn cast(comptime Int: type, value: anytype) Int {
-    return @intCast(Int, value);
+fn rotl(n: anytype, c: anytype) @TypeOf(n) {
+    return std.math.rotl(@TypeOf(n), n, c);
 }
 
-fn truncate(comptime Int: type, value: anytype) Int {
-    return @truncate(Int, value);
+fn rotr(n: anytype, c: anytype) @TypeOf(n) {
+    return std.math.rotr(@TypeOf(n), n, c);
 }
 
-pub const DefaultQueue = Queue(u64, 6, 4);
+const Node = extern struct {
+    prev: ?*Node = null,
+    next: ?*Node = null,
+    last: *Node = undefined,
 
-pub fn Queue(
+    const Queue = extern struct {
+        head: ?*Node = null,
+
+        fn isEmpty(self: Queue) bool {
+            return self.head == null;
+        }
+
+        fn push(self: *Queue, node: *Node) void {
+            node.next = null;
+            node.last = node;
+
+            if (self.head) |head| {
+                node.prev = head.last;
+                head.last.next = node;
+                head.last = node;
+            } else {
+                node.prev = null;
+                self.head = node;
+            }
+        }
+
+        fn pop(self: *Queue) ?*Node {
+            const node = self.head orelse return null;
+            self.remove(node);
+            return node;
+        }
+
+        fn remove(self: *Queue, node: *Node) void {
+            const head = self.head orelse return;
+
+            if (node.prev) |prev|
+                prev.next = node.next;
+            if (node.next) |next|
+                next.prev = node.prev;
+
+            if (node == head) {
+                self.head = node.next;
+                if (self.head) |new_head|
+                    new_head.last = node.last;
+            } else if (node == head.last) {
+                if (node.prev) |prev|
+                    head.last = prev;
+            }
+
+            node.prev = null;
+            node.next = null;
+            node.last = node;
+        }
+
+        fn consume(self: *Queue, other: *Queue) void {
+            const other_head = other.head orelse return;
+
+            if (self.head) |head| {
+                head.last.next = other_head;
+                head.last = other_head.last;
+            } else {
+                self.head = other_head;
+            }
+
+            other.* = Queue{};
+        }
+    };
+};
+
+pub const DefaultWheel = Wheel(u64, 6, 4);
+
+pub fn Wheel(
     comptime timeout_t: type,
     comptime wheel_bit: comptime_int,
     comptime wheel_num: comptime_int,
 ) type {
-    std.debug.assert(wheel_num > 0);
-    std.debug.assert(wheel_bit > 0);
-    std.debug.assert((1 << (wheel_num * wheel_bit)) - 1 <= maxInt(timeout_t));
-    std.debug.assert(@sizeOf(timeout_t) >= @sizeOf(IntType(false, 1 << wheel_bit)));
+    const abstime_t = timeout_t;
+    const reltime_t = timeout_t;
+    const wheel_t = IntType(false, 1 << wheel_bit);
+    const wheel_slot_t = Log2Int(wheel_t);
 
     const wheel_len = 1 << wheel_bit;
+    const wheel_max = wheel_len - 1;
     const wheel_mask = wheel_len - 1;
-    const wheel_t = IntType(false, wheel_len);
-    const wheel_slot_t = IntType(false, wheel_bit);
-    const wheel_num_t = Log2Int(IntType(false, wheel_num));
+    const timeout_max = (@as(timeout_t, 1) << (wheel_bit * wheel_num)) - 1;
 
     return extern struct {
         const Self = @This();
 
-        pub const Entry = List.Node;
+        pub const Tick = timeout_t;
 
-        const List = extern struct {
-            const Node = extern struct {
-                prev: ?*Entry,
-                next: ?*Entry,
-                last: *Entry,
-                list: ?*List,
-                expires: timeout_t,
-            };
+        pub const Entry = extern struct {
+            node: Node = Node{},
+            queue: ?*Node.Queue = null,
+            expires: timeout_t = 0,
 
-            head: ?*Entry = null,
-
-            fn isEmpty(self: List) bool {
-                return self.head == null;
-            }
-
-            fn push(self: *List, entry: *Entry) void {
-                entry.next = null;
-                entry.last = entry;
-                if (self.head) |head| {
-                    entry.prev = head.last;
-                    head.last.next = entry;
-                    head.last = entry;
-                } else {
-                    entry.prev = null;
-                    self.head = entry;
-                }
-            }
-
-            fn pop(self: *List) ?*Entry {
-                const entry = self.head orelse return null;
-                self.remove(entry);
-                return entry;
-            }
-
-            fn remove(self: *List, entry: *Entry) void {
-                const head = self.head orelse return;
-
-                if (entry.prev) |prev| {
-                    prev.next = entry.next;
-                    if (head.last == entry)
-                        head.last = prev;
-                }
-                
-                if (entry.next) |next|
-                    next.prev = entry.prev;
-                if (head == entry)
-                    self.head = entry.next;
-
-                entry.next = null;
-                entry.prev = null;
-                entry.last = entry;
-            }
-
-            fn consume(self: *List, other: *List) void {
-                const other_head = other.head orelse return;
-
-                if (self.head) |head| {
-                    other_head.prev = head.last;
-                    head.last.next = other_head;
-                    head.last = other_head.last;
-                } else {
-                    self.* = other.*;
-                }
-
-                other.* = List{};
+            fn fromNode(node: *Node) *Entry {
+                return @fieldParentPtr(Entry, "node", node);
             }
         };
 
-        wheel: [wheel_num][wheel_len]List = [_][wheel_len]List{[_]List{List{}} ** wheel_len} ** wheel_num,
+        wheel: [wheel_num][wheel_len]Node.Queue = [_][wheel_len]Node.Queue{[_]Node.Queue{Node.Queue{}} ** wheel_len} ** wheel_num,
         pending: [wheel_num]wheel_t = [_]wheel_t{0} ** wheel_num,
-        expired: List = List{},
-        current: timeout_t = 0,
+        expired: Node.Queue = Node.Queue{},
+        curtime: timeout_t = 0,
 
         pub fn now(self: Self) timeout_t {
-            return self.current;
+            return self.curtime;
         }
 
-        pub fn expireAfter(self: *Self, entry: *Entry, duration: timeout_t) void {
-            self.expireAt(entry, self.now() + duration);
+        pub fn remove(self: *Self, entry: *Entry) void {
+            const queue = entry.queue orelse return;
+            const node = &entry.node;
+
+            entry.queue = null;
+            queue.remove(node);
+
+            if ((queue != &self.expired) and queue.isEmpty()) {
+                const index = (@ptrToInt(queue) - @ptrToInt(&self.wheel[0][0])) / @sizeOf(Node.Queue);
+                const wheel = index / wheel_len;
+                const slot = index % wheel_len;
+                self.pending[wheel] &= ~(@as(wheel_t, 1) << cast(wheel_slot_t, slot));
+            }
         }
+        
+        pub const expireAt = schedule;
+        pub fn schedule(self: *Self, entry: *Entry, expires: timeout_t) void {
+            const Timeout = struct {
+                fn rem(_self: *Self, _entry: *Entry) reltime_t {
+                    return _entry.expires - _self.curtime;
+                }
 
-        pub fn expireAt(self: *Self, entry: *Entry, deadline: timeout_t) void {
-            const current = self.now();
+                fn wheel(_timeout: timeout_t) timeout_t {
+                    std.debug.assert(_timeout > 0);
+                    const w = fls(min(_timeout, timeout_max));
+                    return (w - 1) / wheel_bit;
+                }
 
-            var list: *List = undefined;
-            if (deadline <= current) {
-                list = &self.expired;
+                fn slot(_wheel: timeout_t, _expires: timeout_t) wheel_slot_t {
+                    const shift = cast(Log2Int(timeout_t), _wheel) * wheel_bit;
+                    return truncate(wheel_slot_t, (_expires >> shift) - boolToInt(_wheel != 0));
+                }
+            };
+
+            entry.expires = expires;
+
+            var new_queue: *Node.Queue = undefined;
+            if (expires > self.curtime) {
+                const rem = Timeout.rem(self, entry);
+                const wheel = Timeout.wheel(rem);
+                const slot = Timeout.slot(wheel, entry.expires);
+                self.pending[cast(usize, wheel)] |= @as(wheel_t, 1) << slot;
+                new_queue = &self.wheel[cast(usize, wheel)][cast(usize, slot)];
             } else {
-                const rem = deadline - current;
-                const wheel = truncate(wheel_num_t, (cast(Log2Int(timeout_t), fls(min(rem, maxInt(timeout_t)))) - 1) / wheel_bit);
-                const slot = truncate(wheel_slot_t, (deadline >> (cast(Log2Int(timeout_t), wheel) * wheel_bit)) - boolToInt(wheel != 0));
-                self.pending[wheel] |= as(wheel_t, 1) << slot;
-                list = &self.wheel[wheel][slot];
+                new_queue = &self.expired;
             }
 
-            entry.list = list;
-            entry.expires = deadline;
-            list.push(entry);
+            entry.queue = new_queue;
+            new_queue.push(&entry.node);
         }
 
-        pub const remove = invalidate;
-        pub fn invalidate(self: *Self, entry: *Entry) void {
-            const list = entry.list orelse return;
-            entry.list = null;
-            list.remove(entry);
-
-            if ((list != &self.expired) and list.isEmpty()) {
-                const index = (@ptrToInt(list) - @ptrToInt(&self.wheel[0][0])) / @sizeOf(List);
-                const wheel = cast(wheel_num_t, index / wheel_len);
-                const slot = cast(wheel_slot_t, index % wheel_len);
-                self.pending[wheel] &= ~(as(wheel_t, 1) << slot);
-            }
+        pub const expireAfter = add;
+        pub fn add(self: *Self, entry: *Entry, timeout: reltime_t) void {
+            self.schedule(entry, self.now() + timeout);
         }
 
-        pub fn advance(self: *Self, duration: timeout_t) void {
-            self.update(self.now() + duration);
-        }
-
-        pub fn update(self: *Self, new_now: timeout_t) void {
-            const old_now = self.now();
-            self.current = new_now;
+        pub fn update(self: *Self, new_now: abstime_t) void {
+            const old_now = self.curtime;
+            self.curtime = new_now;
             if (new_now <= old_now)
                 return;
 
-            var processed = List{};
-            var processed_last: ?*Entry = null;
+            var processed = Node.Queue{};
             var elapsed = new_now - old_now;
 
             for (self.pending) |*mask, wheel| {
-                var pending_slots: wheel_t = undefined;
+                var pending: wheel_t = undefined;
+                const shift = cast(Log2Int(timeout_t), wheel * wheel_bit);
 
-                const offset = cast(Log2Int(timeout_t), wheel) * wheel_bit;
-                if ((elapsed >> offset) > wheel_mask) {
-                    pending_slots = ~as(wheel_t, 0);
+                if ((elapsed >> shift) > wheel_max) {
+                    pending = ~@as(wheel_t, 0);
                 } else {
-                    const _elapsed = truncate(wheel_slot_t, elapsed >> offset);
-                    const _rslot = (as(wheel_t, 1) << _elapsed) - 1;
+                    const _elapsed = truncate(wheel_t, elapsed >> shift);
+                    const _elapsed_rot = (@as(wheel_t, 1) << cast(wheel_slot_t, _elapsed)) - 1; 
 
-                    const _oslot = truncate(wheel_slot_t, old_now >> offset);
-                    pending_slots = rotl(wheel_t, _rslot, cast(wheel_t, _oslot));
+                    const oslot = truncate(wheel_t, old_now >> shift);
+                    pending = rotl(_elapsed_rot, oslot);
 
-                    const _nslot = truncate(wheel_slot_t, new_now >> offset);
-                    const _nslotl = rotl(wheel_t, _rslot, cast(wheel_t, _nslot));
-                    const _nslotlr = rotr(wheel_t, _nslotl, cast(wheel_t, _elapsed));
-                    pending_slots |= _nslotlr | (as(wheel_t, 1) << _nslot);
+                    const nslot = truncate(wheel_t, new_now >> shift);
+                    pending |= rotr(rotl(_elapsed_rot, nslot), _elapsed);
+                    pending |= @as(wheel_t, 1) << cast(wheel_slot_t, nslot);
                 }
 
-                while ((pending_slots & mask.*) != 0) {
-                    const slot = truncate(wheel_slot_t, ctz(pending_slots & mask.*));
-                    mask.* &= ~(as(wheel_t, 1) << slot);
-                    processed.consume(&self.wheel[wheel][slot]);
+                while ((pending & mask.*) != 0) {
+                    const slot = cast(wheel_slot_t, ctz(pending & mask.*));
+                    const queue = &self.wheel[cast(usize, wheel)][cast(usize, slot)];
+                    processed.consume(queue);
+                    mask.* &= ~(@as(wheel_t, 1) << slot);
                 }
 
-                elapsed = max(elapsed, as(timeout_t, wheel_len) << offset);
-                if ((pending_slots & 0x1) == 0)
+                if ((pending & 0x1) == 0) {
                     break;
+                } else {
+                    elapsed = max(elapsed, @as(timeout_t, wheel_len) << shift);
+                }
             }
 
-            while (processed.pop()) |entry| {
-                if (entry.list) |list|
-                    list.remove(entry);
-                self.expireAt(entry, entry.expires);
+            while (processed.pop()) |node| {
+                const entry = Entry.fromNode(node);
+                self.remove(entry);
+                self.schedule(entry, entry.expires);
             }
+        }
+
+        pub const advance = step;
+        pub fn step(self: *Self, elapsed: reltime_t) void {
+            self.update(self.now() + elapsed);
+        }
+
+        pub fn hasExpired(self: Self) bool {
+            return !self.expired.isEmpty();
+        }
+
+        pub fn getExpired(self: *Self) ?*Entry {
+            const node = self.expired.pop() orelse return null;
+            return Entry.fromNode(node);
+        }
+
+        pub fn hasPending(self: Self) bool {
+            const Vector = std.meta.Vector(wheel_num, wheel_t);
+            var zeroes: Vector = @splat(wheel_num, @as(wheel_t, 0));
+            const pending: Vector = self.pending;
+
+            for (@bitCast([wheel_num]bool, pending == zeroes)) |wheel_has_pending| {
+                if (wheel_has_pending) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        pub fn getPendingEstimate(self: Self) ?reltime_t {
+            var timeout = ~@as(reltime_t, 0);
+            var relmask: reltime_t = 0;
+            var has_timeout = false;
+
+            for (self.pending) |mask, wheel| {
+                const shift = cast(Log2Int(timeout_t), wheel) * wheel_bit;
+                var _timeout: timeout_t = undefined;
+
+                if (mask != 0) {
+                    const slot = truncate(wheel_slot_t, self.curtime >> shift);
+                    const tmp = ctz(rotr(mask, @as(wheel_t, slot)));
+                    _timeout = @as(timeout_t, tmp + boolToInt(wheel != 0)) << shift;
+                    _timeout -= relmask & self.curtime;
+                    timeout = min(_timeout, timeout);
+                    has_timeout = true;
+                }
+
+                relmask <<= wheel_bit;
+                relmask |= wheel_mask;
+            }
+
+            if (!has_timeout)
+                return null;
+            return timeout;
         }
 
         pub const Poll = union(enum) {
             expired: *Entry,
-            wait_for: timeout_t,
+            wait_for: Tick,
         };
 
         pub fn poll(self: *Self) ?Poll {
-            if (self.get()) |entry| {
-                return Poll{ .expired = entry };
+            if (self.getExpired()) |expired| {
+                return Poll{ .expired = expired };
             }
 
-            if (self.nextExpire()) |expires_in| {
-                return Poll{ .wait_for = expires_in }; 
+            if (self.getPendingEstimate()) |wait_for| {
+                return Poll{ .wait_for = wait_for };
             }
 
-            return null;
-        }
-
-        fn get(self: *Self) ?*Entry {
-            return self.expired.pop();
-        }
-
-        fn nextExpire(self: Self) ?timeout_t {
-            const current = self.now();
-            var rel_mask = as(timeout_t, 0);
-            var interval = ~rel_mask;
-            var has_timeout = false;
-            
-            for (self.pending) |mask, wheel| {
-                if (mask != 0) {
-                    const shift = cast(Log2Int(timeout_t), wheel) * wheel_bit;
-                    const slot = truncate(wheel_t, current >> shift);
-                    const offset = ctz(rotr(wheel_t, mask, as(wheel_t, slot)));
-                    const rotated = as(timeout_t, offset + boolToInt(wheel != 0)) << shift;
-                    const timeout = rotated - (rel_mask & current);
-                    
-                    if (timeout != 0) {
-                        interval = min(interval, timeout);
-                        has_timeout = true;
-                    }
-                }
-
-                rel_mask <<= wheel_bit;
-                rel_mask |= wheel_mask;
-            }
-
-            if (has_timeout)
-                return interval;
             return null;
         }
     };
--- tests/timer.zig
+++ tests/timer.zig
@@ -18,38 +18,45 @@ const zap = @import("zap");
 const assert = std.testing.expect;
 const assertEq = std.testing.expectEqual;
 
-const TimerQueue = zap.core.Timer.Queue;
-const DefaultTimerQueue = zap.core.Timer.DefaultQueue;
+const Wheel = zap.core.Timer.Wheel;
+const DefaultWheel = zap.core.Timer.DefaultWheel;
 
 test "timer wheel queue" {
     inline for ([_]type {
-        DefaultTimerQueue,
+        DefaultWheel,
         // different bit sizes
-        TimerQueue(u128, 7, 4),
-        TimerQueue(u64, 6, 4),
-        TimerQueue(u64, 5, 4),
-        TimerQueue(u64, 4, 4),
-        TimerQueue(u64, 3, 4),
+        Wheel(u128, 7, 4),
+        Wheel(u64, 6, 4),
+        Wheel(u64, 5, 4),
+        Wheel(u64, 4, 4),
+        Wheel(u64, 3, 4),
         // different tick sizes
-        TimerQueue(u128, 3, 4),
-        TimerQueue(u32, 3, 4),
-        //TimerQueue(u16, 3, 4),
-    }) |Queue| {
-        const Poll = Queue.Poll;
-        const Entry = Queue.Entry;
+        Wheel(u128, 3, 4),
+        Wheel(u32, 3, 4),
+        Wheel(u16, 3, 4),
+    }) |WheelType| {
+        const Entry = WheelType.Entry;
+        const Poll = WheelType.Poll;
 
-        var queue = Queue{};
+        var queue = WheelType{};
         assertEq(@as(?Poll, null), queue.poll());
         assertEq(@as(?Poll, null), queue.poll());
 
         var _5: Entry = undefined;
         queue.expireAfter(&_5, 5);
+        assert(queue.hasPending());
+        assert(!queue.hasExpired());
         assertEq(@as(?Poll, Poll{.wait_for = 5}), queue.poll());
 
         queue.advance(3);
+        assert(queue.hasPending());
+        assert(!queue.hasExpired());
         assertEq(@as(?Poll, Poll{.wait_for = 5 - 3}), queue.poll());
 
         queue.advance(2);
+        assert(!queue.hasPending());
+        assert(queue.hasExpired());
+
         assertEq(@as(?Poll, Poll{.expired = &_5}), queue.poll());
         assertEq(@as(?Poll, null), queue.poll());
         assertEq(@as(?Poll, null), queue.poll());
@@ -69,8 +76,10 @@ test "timer wheel queue" {
         queue.expireAfter(&_10000, 10000);
 
         assertEq(@as(?Poll, Poll{.expired = &_past}), queue.poll());
-        assertEq(@as(?Poll, Poll{.wait_for = 10}), queue.poll());
 
+        const p10 = queue.poll();
+        assert(p10 != null);
+        assert(p10.?.wait_for <= 10);
         queue.update(queue.now() + 10);
         assertEq(@as(?Poll, Poll{.expired = &_10}), queue.poll());
         assertEq(@as(?Poll, Poll{.expired = &_10_2}), queue.poll());

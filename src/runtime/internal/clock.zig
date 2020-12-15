a04ebe19d87b129d42dba9a7e7944f41960f93bf
kprotty <kbutcher6200@gmail.com>
2020-12-15 15:07:17 -0600

    more planning + windows fast nanotime & event primitive

--- src/runtime/internal/clock.zig
+++ src/runtime/internal/clock.zig
@@ -1,5 +1,5 @@
 const builtin = @import("builtin");
-const system = @import("./system.zig");
+const atomic = @import("../../sync/atomic.zig");
 
 pub usingnamespace switch (builtin.os.tag) {
     .linux => LinuxClock,
@@ -9,19 +9,69 @@ pub usingnamespace switch (builtin.os.tag) {
     else => @compileError("OS not supported for monotonic timing"),
 };
 
-const DarwinClock = struct {
-    pub const is_monotonic = true;
-
+const WindowsClock = struct {
     pub fn nanotime() u64 {
-        var info: system.mach_timebase_info_data_t = undefined;
-        system.mach_timebase_info(&info);
+        // https://www.geoffchappell.com/studies/windows/km/ntoskrnl/structs/kuser_shared_data/index.htm
+        const PINTERRUPT_TIME = @intToPtr(
+            *volatile extern struct {
+                UserBits: u64,
+                KernelUpperBits: u32,
+            },
+            0x7FFE0000 + 0x8,
+        );
+
+        while (true) {
+            // loads guaranteed to be in order due to volatile accesses
+            const kernel_now = PINTERRUPT_TIME.UserBits;
+            const kernel_high = PINTERRUPT_TIME.KernelUpperBits;
+
+            // non-matching high bits means the kernel wrote a new value
+            const now_high = @intCast(u32, kernel_now >> 32);
+            if (now_high != kernel_high)
+                continue;
+
+            // kernel value stored as units of 100ns
+            return kernel_now *% 100;
+        }
+    }
+};
+
+const DarwinClock = struct {
+    pub fn nanotime() u64 {
+        // This does global caching for us internally
+        var info: mach_timebase_info_data_t = undefined;
+        if (mach_timebase_info(&info) != 0)
+            unreachable;
         
-        var now = system.mach_absolute_time();
+        // Only do the scaling if there is some to be done (as mul/div can be expensive)
+        var now = mach_absolute_time();
         if (info.numer != 1)
             now *= info.numer;
         if (info.denom != 1)
             now /= info.denom;
 
-        return now;  
+        return now;
+    }
+
+    const mach_timebase_info_data_t = extern struct {
+        numer: u32,
+        denom: u32,
+    };
+
+    extern "c" fn mach_absolute_time() callconv(.C) u64;
+    extern "c" fn mach_timebase_info(
+        data: ?*mach_timebase_info_data_t,
+    ) callconv(.C) c_int;
+};
+
+const PosixClock = struct {
+    pub fn nanotime() u64 {
+
+    }
+};
+
+const LinuxClock = struct {
+    pub fn nanotime() u64 {
+
     }
 };
--- src/runtime/internal/event.zig
+++ src/runtime/internal/event.zig
@@ -1,5 +1,4 @@
 const builtin = @import("builtin");
-const system = @import("./system.zig");
 const atomic = @import("../../sync/atomic.zig");
 const nanotime = @import("./clock.zig").nanotime;
 
@@ -14,158 +13,93 @@ pub const Event = switch (builtin.os.tag) {
     else => @compileError("OS not supported for thread blocking/unblocking"),
 };
 
+// futex
 const LinuxEvent = @compileError("TODO");
 
+// lwp_park
 const NetBSDEvent = @compileError("TODO");
 
+// thrsleep/thrwakeup
 const OpenBSDEvent = @compileError("TODO");
 
+// umtx_op
 const FreeBSDEvent = @compileError("TODO");
 
+// sys_umtx_op
 const DragonflyEvent = @compileError("TODO");
 
-const WindowsEvent = ConditionEvent(struct {
-    srwlock: system.SRWLOCK,
-    condvar: system.CONDITION_VARIABLE,
+// pthread with static init but no destroy as not needed
+const DarwinEvent = @compileError("TODO");
 
-    const Self = @This();
+// NtWaitForAlertByThreadId / NtAlertThreadByThreadId 
+// (functions used in WaitOnAddress / SRWLOCK / CONDITION_VARIABLE)
+const WindowsEvent = extern struct {
+    thread_id: u32 = 0,
 
-    pub fn init(self: *Self) void {
-        self.srwlock = system.SRWLOCK_INIT;
-        self.condvar = system.CONDITION_VARIABLE_INIT;
-    }
+    pub fn wait(self: *Event, deadline: ?u64, condition: anytype) error{TimedOut}!void {
+        if (self.thread_id == 0)
+            self.thread_id = GetCurrentThreadIdFast();
+        
+        if (!condition.wait())
+            return;
 
-    pub fn deinit(self: *Self) void {
-        self.* = undefined;
-    }
-
-    pub fn lock(self: *Self) void {
-        system.AcquireSRWLockExclusive(&self.srwlock);
-    }
-
-    pub fn unlock(self: *Self) void {
-        system.ReleaseSRWLockExclusive(&self.srwlock);
-    }
-
-    pub fn wait(self: *Self, timeout: ?u64) void {
-        var timeout_ms = system.INFINITE;
-        if (timeout) |timeout_ns| {
-            const ms = @divFloor(timeout_ns, 1_000_000);
-            if (ms < @as(u64, ~@as(@TypeOf(timeout_ms), 0)))
-                timeout_ms = @intCast(@TypeOf(timeout_ms), ms);
+        // Set a timeout if a valid deadline was provided.
+        // Timeouts are LARGE_INTEGERs representing units of 100ns 
+        // where a negative value means a relative offset.
+        var timeout: i64 = undefined;
+        var timeout_ptr: ?*const i64 = null;
+        if (deadline) |deadline_ns| {
+            const now = nanotime();
+            if (now > deadline_ns)
+                return error.TimedOut;
+            timeout_ptr = &timeout;
+            timeout = -@intCast(i64, @divFloor((now - deadline_ns), 100);
         }
 
-        const status = system.SleepConditionVariableSRW(&self.condvar, &mutex.srwlock, timeout, 0);
-        if (status == system.FALSE) {
-            if (system.GetLastError() != system.ERROR_TIMEOUT)
-                unreachable;
-        }
-    }
-
-    pub fn notify(self: *Self) void {
-        system.WakeConditionVariable(&self.condvar);
-    }
-});
-
-const DarwinEvent = ConditionEvent(struct {
-    const Self = @This();
-
-    pub fn init(self: *Self) void {
-        @compileError("TODO");
-    }
-
-    pub fn deinit(self: *Self) void {
-        @compileError("TODO");
-    }
-
-    pub fn lock(self: *Self) void {
-        @compileError("TODO");
-    }
-
-    pub fn unlock(self: *Self) void {
-        @compileError("TODO");
-    }
-
-    pub fn wait(self: *Self, timeout: ?u64) void {
-        @compileError("TODO");
-    }
-
-    pub fn notify(self: *Self) void {
-        @compileError("TODO");
-    }
-});
-
-fn ConditionEvent(comptime Condition: type) type {
-    return struct {
-        state: State = undefined,
-        condition: Condition = undefined,
-
-        const Self = @This();
-        const State = enum {
-            empty = 0,
-            waiting,
-            notified,
+        const deadline_ns = switch (NtWaitForAlertByThreadId(0, timeout_ptr)) {
+            STATUS_ALERTED => return,
+            STATUS_TIMEOUT => deadline_ns orelse unreachable,
+            else => unreachable,
         };
+        
+        const now = nanotime();
+        if (now > deadline_ns)
+            return error.TimedOut;
+    }
 
-        pub fn init(self: *Self) void {
-            self.state = .empty,
-            self.condition.init();
-        }
+    pub fn notify(self: *Event) void {
+        const status = NtAlertThreadByThreadId(self.thread_id);
+        if (status != 0)
+            unreachable;
+    }
 
-        pub fn deinit(self: *Self) void {
-            if (self.state == .waiting)
-                unreachable;
-            self.condition.deinit();
-        }
+    inline fn GetCurrentThreadIdFast() u32 {
+        // https://en.wikipedia.org/wiki/Win32_Thread_Information_Block
+        return switch (builtin.arch) {
+            .i386 => asm volatile (
+                \\ movl %%fs:0x24, %[tid]
+                : [tid] "=r" (-> u32)
+            ),
+            .x86_64 => asm volatile (
+                \\ movl %%gs:0x48, %[tid]
+                : [tid] "=r" (-> u32)
+            ),
+            else => GetCurrentThreadId(),
+        };
+    }
 
-        pub fn wait(self: *Self, deadline: ?u64, condition: anytype) bool {
-            self.condition.lock();
-            defer self.condition.unlock();
+    const STATUS_ALERTED = 0x101;
+    const STATUS_TIMEOUT = 0x102;
+    const WINAPI = if (builtin.arch == .i386) .Stdcall else .C;
 
-            if (!condition.wait())
-                return true;
+    extern "kernel32" fn GetCurrentThreadId() callconv(WINAPI) u32;
+    extern "NtDll" fn NtAlertThreadByThreadId(
+        thread_id: usize,
+    ) callconv(WINAPI) u32;
+    extern "NtDll" fn NtWaitForAlertByThreadId(
+        address: usize,
+        timeout: ?*const i64,
+    ) callconv(WINAPI) u32;
+    
+};
 
-            switch (self.state) {
-                .empty => self.state = .waiting,
-                .waiting => unreachable,
-                .notified => return true,
-            }
-
-            while (true) {
-                switch (self.state) {
-                    .empty => unreachable,
-                    .waiting => {},
-                    .notified => return true,
-                }
-
-                const deadline_ns = deadline orelse {
-                    self.condition.wait(null);
-                    continue;
-                };
-
-                const now = nanotime();
-                if (now > deadline_ns) {
-                    self.state = .empty;
-                    return false;
-                }
-
-                const timeout_ns = deadline_ns - now;
-                self.condition.wait(timeout_ns)
-            }
-        }
-
-        pub fn notify(self: *Self) void {
-            self.condition.lock();
-            defer self.condition.unlock();
-
-            const state = self.state;
-            self.state = .notified;
-
-            switch (state) {
-                .empty => {},
-                .waiting => self.condition.notify(),
-                .notified => unreachable,
-            }
-        }
-    };
-}

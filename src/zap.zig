258900a2944901651d94c4b1e372f96e9353780a
lithdew <kenta@lithdew.net>
2021-03-19 11:44:33 +0900

    zap: export package

---
+++ src/zap.zig
@@ -0,0 +1,4 @@
+pub const Futex = @import("runtime/Futex.zig");
+pub const Lock = @import("runtime/Lock.zig");
+pub const Pool = @import("runtime/Pool.zig");
+pub const Thread = @import("runtime/Thread.zig");

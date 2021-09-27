dee3ed107201a1f61d51d869078dac0c6dfba861
kprotty <kbutcher6200@gmail.com>
2021-09-27 11:30:34 -0500

    switch IdleQueue to low_level Lock+Event

--- src/runtime/idle.rs
+++ src/runtime/idle.rs
@@ -1,38 +1,110 @@
-use std::sync::{Condvar, Mutex};
+use super::super::sync::low_level::{AutoResetEvent, Lock};
+use std::{cell::Cell, pin::Pin, ptr::NonNull};
+
+struct IdleWaiter {
+    next: Cell<Option<NonNull<Self>>>,
+    event: AutoResetEvent,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum IdleState {
+    Empty,
+    Waiting(NonNull<IdleWaiter>),
+    Notified,
+    Shutdown,
+}
+
+unsafe impl Send for IdleState {}
+
+impl Default for IdleState {
+    fn default() -> Self {
+        Self::Empty
+    }
+}
 
 #[derive(Default)]
 pub struct IdleQueue {
-    count: Mutex<usize>,
-    cond: Condvar,
+    state: Lock<IdleState>,
 }
 
 impl IdleQueue {
     pub fn wait(&self, validate: impl Fn() -> bool) {
-        let mut count = self.count.lock().unwrap();
-        loop {
+        let waiter = IdleWaiter {
+            next: Cell::new(None),
+            event: AutoResetEvent::default(),
+        };
+
+        let waiter = unsafe { Pin::new_unchecked(&waiter) };
+        let is_waiting = self.state.with(|state| {
             if !validate() {
-                return;
+                return false;
             }
 
-            if *count > 0 {
-                *count -= 1;
-                return;
+            let ptr = NonNull::from(&*waiter);
+            match *state {
+                IdleState::Empty => {
+                    *state = IdleState::Waiting(ptr);
+                    true
+                }
+                IdleState::Waiting(head) => {
+                    waiter.next.set(Some(head));
+                    *state = IdleState::Waiting(ptr);
+                    true
+                }
+                IdleState::Notified => {
+                    *state = IdleState::Empty;
+                    false
+                }
+                IdleState::Shutdown => false,
             }
+        });
 
-            count = self.cond.wait(count).unwrap();
+        if is_waiting {
+            unsafe {
+                Pin::map_unchecked(waiter, |w| &w.event).wait();
+            }
         }
     }
 
     pub fn signal(&self) -> bool {
-        let mut count = self.count.lock().unwrap();
-        *count += 1;
-        self.cond.notify_one();
-        *count == 1
+        self.state
+            .with(|state| unsafe {
+                match *state {
+                    IdleState::Empty => {
+                        *state = IdleState::Notified;
+                        None
+                    }
+                    IdleState::Waiting(head) => {
+                        *state = match head.as_ref().next.get() {
+                            Some(new_head) => IdleState::Waiting(new_head),
+                            None => IdleState::Empty,
+                        };
+                        Some(head)
+                    }
+                    IdleState::Notified => None,
+                    IdleState::Shutdown => None,
+                }
+            })
+            .map(|waiter| unsafe {
+                Pin::new_unchecked(&waiter.as_ref().event).notify();
+                true
+            })
+            .unwrap_or(false)
     }
 
     pub fn shutdown(&self) {
-        let mut count = self.count.lock().unwrap();
-        *count = usize::MAX;
-        self.cond.notify_all();
+        let mut state = self
+            .state
+            .with(|state| std::mem::replace(state, IdleState::Shutdown));
+
+        while let IdleState::Waiting(waiter) = state {
+            unsafe {
+                state = match waiter.as_ref().next.get() {
+                    Some(new_waiter) => IdleState::Waiting(new_waiter),
+                    None => IdleState::Empty,
+                };
+                Pin::new_unchecked(&waiter.as_ref().event).notify();
+            }
+        }
     }
 }

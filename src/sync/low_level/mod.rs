1e5389cabb601a6e42750491b289ea8afe90f656
kprotty <kbutcher6200@gmail.com>
2021-09-27 22:24:39 -0500

    WaitQueue compiles

--- src/sync/low_level/mod.rs
+++ src/sync/low_level/mod.rs
@@ -1,9 +1,11 @@
 mod event;
 mod lock;
 mod once;
+mod wait_queue;
 mod waker;
 
 pub use event::AutoResetEvent;
 pub use lock::Lock;
 pub use once::Once;
+pub use wait_queue::{WaitQueue, WaitToken, WakeToken};
 pub use waker::{AtomicWaker, WakerUpdate};
--- src/sync/low_level/wait_queue.rs
+++ src/sync/low_level/wait_queue.rs
@@ -1,7 +1,11 @@
-use super::{Lock, AtomicWaker};
+use super::{AtomicWaker, AutoResetEvent, Lock, WakerUpdate};
 use std::{
     cell::Cell,
+    future::Future,
+    marker::PhantomPinned,
+    pin::Pin,
     ptr::NonNull,
+    task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
 };
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -49,13 +53,17 @@ impl<'a> WaitList<'a> {
         WaitListIter { current: self.head }
     }
 
+    fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
     unsafe fn insert(&mut self, node: NonNull<WaitNode>) {
-        assert_eq!(node.key, self.key);
-        assert!(!node.inserted.get());
-        
+        assert_eq!(node.as_ref().key, self.key);
+        assert!(!node.as_ref().inserted.get());
+
         node.as_ref().next.set(None);
         node.as_ref().inserted.set(true);
-        
+
         if let Some(head) = self.head {
             let tail = head.as_ref().tail.get().expect("head without tail");
             tail.as_ref().next.set(Some(node));
@@ -66,7 +74,7 @@ impl<'a> WaitList<'a> {
             node.as_ref().tail.set(Some(node));
             node.as_ref().key_next.set(None);
             node.as_ref().key_prev.set(self.key_prev);
-            
+
             self.head = Some(node);
             if let Some(key_prev) = self.key_prev {
                 key_prev.as_ref().key_next.set(Some(node));
@@ -90,11 +98,10 @@ impl<'a> WaitList<'a> {
             assert_ne!(node, head);
             prev.as_ref().next.set(next);
             if let Some(next) = next {
-                next.as_ref().prev.set(prev);
+                next.as_ref().prev.set(Some(prev));
             } else {
-                assert_eq!(node, head.as_ref().tail.get());
-                assert!(prev.is_some());
-                head.as_ref().tail.set(prev);
+                assert_eq!(Some(node), head.as_ref().tail.get());
+                head.as_ref().tail.set(Some(prev));
             }
             return true;
         }
@@ -134,21 +141,22 @@ struct WaitBucket {
 }
 
 impl WaitBucket {
+    const fn new() -> Self {
+        Self { root: None }
+    }
+
     pub fn list_for(&mut self, key: usize) -> WaitList<'_> {
         let mut head = self.root;
         let mut key_prev = None;
 
         while let Some(waiter) = head {
-            let (wait_key, wait_key_next) = unsafe {
-                (waiter.as_ref().key, waiter.as_ref.key_next.get())
-            };
-
-            if wait_key == key {
+            let waiter = unsafe { waiter.as_ref() };
+            if waiter.key == key {
                 break;
             }
 
-            key_prev = Some(waiter);
-            head = wait_key_next;
+            key_prev = head;
+            head = waiter.key_next.get();
         }
 
         WaitList {
@@ -171,10 +179,10 @@ impl WaitQueue {
         }
     }
 
-    pub async fn wait<V, C>(&self, key: usize, validate: V, cancelled: C) -> Option<WakeToken> 
+    pub async fn wait<V, C>(&self, key: usize, validate: V, cancelled: C) -> Option<WakeToken>
     where
         V: FnOnce() -> Option<WaitToken>,
-        C: FnOnce(WaitToken, bool),
+        C: FnOnce(WaitToken, bool) + Unpin,
     {
         struct WaitFuture<'a, 'b, C: FnOnce(WaitToken, bool)> {
             node: Option<Pin<&'a WaitNode>>,
@@ -184,10 +192,55 @@ impl WaitQueue {
 
         impl<'a, 'b, C: FnOnce(WaitToken, bool)> WaitFuture<'a, 'b, C> {
             #[cold]
-            fn drop_slow(&self, node: Pin<&'a WaitNode>) {
-                self.queue.bucket.with(|bucket| {
-                    
-                })
+            fn drop_slow(&mut self, node: Pin<&'a WaitNode>) {
+                if node.waker.update(None) == WakerUpdate::Notified {
+                    return;
+                }
+
+                let removed = self.queue.bucket.with(|bucket| unsafe {
+                    let mut list = bucket.list_for(node.key);
+                    if !list.try_remove(NonNull::from(&*node)) {
+                        return false;
+                    }
+
+                    let cancelled = self.cancelled.take().unwrap();
+                    cancelled(node.token.get(), !list.is_empty());
+                    true
+                });
+
+                if !removed {
+                    Self::wait_for_wake(node)
+                }
+            }
+
+            #[cold]
+            fn wait_for_wake(node: Pin<&'a WaitNode>) {
+                const EVENT_VTABLE: RawWakerVTable = RawWakerVTable::new(
+                    |ptr| RawWaker::new(ptr, &EVENT_VTABLE),
+                    |ptr| unsafe {
+                        let event = &*(ptr as *const AutoResetEvent);
+                        Pin::new_unchecked(event).notify();
+                        // ^ potentially dangling reference but
+                        // rust stdlib does this too & theres no obvious fix
+                    },
+                    |_ptr| unreachable!("wake_by_ref() was called waiting for WaitNode"),
+                    |_ptr| {},
+                );
+
+                unsafe {
+                    let event = AutoResetEvent::default();
+                    let event = Pin::new_unchecked(&event);
+
+                    let ptr = &*event as *const AutoResetEvent as *const ();
+                    let raw_waker = RawWaker::new(ptr, &EVENT_VTABLE);
+                    let waker = Waker::from_raw(raw_waker);
+
+                    match node.waker.update(Some(&waker)) {
+                        WakerUpdate::New => event.wait(),
+                        WakerUpdate::Replaced => unreachable!("waker should have been emptied"),
+                        WakerUpdate::Notified => {}
+                    }
+                }
             }
         }
 
@@ -199,16 +252,111 @@ impl WaitQueue {
             }
         }
 
-        impl<'a, 'b, C: FnOnce(WaitToken, bool)> Future for WaitFuture<'a, 'b, C> {
+        impl<'a, 'b, C: FnOnce(WaitToken, bool) + Unpin> Future for WaitFuture<'a, 'b, C> {
             type Output = WakeToken;
 
-            fn poll(self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Self::Output> {
+            fn poll(mut self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Self::Output> {
+                let node = self
+                    .node
+                    .take()
+                    .expect("WaitFuture polled after completion");
 
+                if node.waker.update(Some(ctx.waker())) == WakerUpdate::Notified {
+                    return Poll::Ready(WakeToken(node.token.get().0));
+                }
+
+                self.node = Some(node);
+                Poll::Pending
+            }
+        }
+
+        let node = WaitNode {
+            key,
+            key_prev: Cell::new(None),
+            key_next: Cell::new(None),
+            prev: Cell::new(None),
+            next: Cell::new(None),
+            tail: Cell::new(None),
+            inserted: Cell::new(false),
+            token: Cell::new(WaitToken(0)),
+            waker: AtomicWaker::default(),
+            _pinned: PhantomPinned,
+        };
+
+        unsafe {
+            let node = Pin::new_unchecked(&node);
+            let is_waiting = self.bucket.with(|bucket| {
+                match validate() {
+                    Some(token) => node.token.set(token),
+                    None => return false,
+                }
+
+                let mut list = bucket.list_for(key);
+                list.insert(NonNull::from(&*node));
+                true
+            });
+
+            if !is_waiting {
+                return None;
+            }
+
+            let wait_future = WaitFuture {
+                node: Some(node),
+                queue: self,
+                cancelled: Some(cancelled),
+            };
+
+            Some(wait_future.await)
+        }
+    }
+
+    pub fn wake(
+        &self,
+        key: usize,
+        mut filter: impl FnMut(WaitToken) -> Option<WakeToken>,
+        before_wake: impl FnOnce(bool),
+    ) {
+        let mut nodes = self.bucket.with(|bucket| {
+            let mut unparked = None;
+            let mut list = bucket.list_for(key);
+
+            for node in list.iter() {
+                let node = unsafe { Pin::new_unchecked(node.as_ref()) };
+                match filter(node.token.get()) {
+                    Some(token) => node.token.set(WaitToken(token.0)),
+                    None => break,
+                }
+
+                let node_ptr = NonNull::from(&*node);
+                assert!(unsafe { list.try_remove(node_ptr) });
+
+                node.next.set(unparked);
+                unparked = Some(node_ptr);
+            }
+
+            before_wake(!list.is_empty());
+            unparked
+        });
+
+        while let Some(node) = nodes {
+            unsafe {
+                nodes = node.as_ref().next.get();
+                node.as_ref().waker.wake();
+                // ^ potentially dangling reference but
+                // rust stdlib does this too & theres no obvious fix
             }
         }
     }
 
-    pub fn wake(&self, key: usize, mut filter: impl FnMut(WaitToken, bool) -> Option<WakeToken>) {
+    pub fn notify(&self, key: usize, mut count: usize, before_wake: impl FnOnce(bool)) {
+        let filter = |_| match count {
+            0 => None,
+            _ => {
+                count -= 1;
+                Some(WakeToken(0))
+            }
+        };
 
+        self.wake(key, filter, before_wake)
     }
 }

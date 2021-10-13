be7e9353f10c0b0fb4c21f1eadd79c1e2e6616c4
kprotty <kbutcher6200@gmail.com>
2021-10-13 14:20:58 -0500

    fix inferred error compile bug

--- http.zig
+++ http.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const os = std.os;
+
 const Loop = @import("src/s3.zig");
 const net = Loop.net;
 
@@ -6,13 +8,12 @@ pub fn main() !void {
     try (try Loop.run(asyncMain, .{}));
 }
 
-fn asyncMain() !void {
-    std.debug.warn("Hello World\n", .{});
-    // // var server = net.StreamServer.init(.{ .reuse_address = true });
-    // // defer server.deinit();
+fn asyncMain() anyerror!void {
+    var server = net.StreamServer.init(.{ .reuse_address = true });
+    defer server.deinit();
 
-    // const address = try net.Address.parseIp("127.0.0.1", 3000);
-    // //try server.listen(address);
+    const address = try net.Address.parseIp("127.0.0.1", 3000);
+    try server.listen(address);
 
-    // std.debug.print("Listening on {}", .{ address });
+    std.debug.print("Listening on {}", .{ address });
 }

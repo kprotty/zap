9d29376accde189d64af6fbc9aa189e72e484095
ethernetsellout <bastedopeter7@gmail.com>
2023-06-05 18:42:47 -0700

    updates benchmark build script

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    _ = b.addModule("zap", .{ .source = .{ .path = "src/thread_pool" } });
-    _ = b.addModule("zap_go", .{ .source = .{ .path = "src/thread_pool_go_based" } });
+    _ = b.addModule("zap", .{ .source_file = .{ .path = "src/thread_pool" } });
+    _ = b.addModule("zap_go", .{ .source_file = .{ .path = "src/thread_pool_go_based" } });
 }
